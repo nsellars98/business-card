7133be00c5b1f7392ee108ec4bc5bcdabeade73c
nsellars98 <nsellars98@gmail.com>
2021-11-25 12:38:35 -0600

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h3 className="about-heading">About</h3>
+      <p 
+        className="about-text">
+          I am a software developer aiming to solve real-world problems and make people's lives better.
+          I prioritze best practices in my current and future work while openly seeking out new challenges and things to learn.
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Interests.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Interests() {
+  return (
+    <div className="interests-container">
+      <h3 className="interests-heading">Interests</h3>
+      <p 
+        className="interests-text">
+          My goal is to get into full stack development.
+          However, I enjoy reading about and researching different technologies like machine learning, AI, and blockchain technologies including Ethereum and Solidity.
+          On the personal side, I like to swim, bike and run!
+      </p>
+    </div>
+  );
+}
+
+export default Interests;
