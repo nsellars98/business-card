005a65290341d32f118a38f3a056d6d633b420e1
nsellars98 <nsellars98@gmail.com>
2021-11-29 16:28:45 -0600

    Finishing touches but still needs responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function About() {
   return (
-    <div className="about-container">
-      <h3 className="about-heading">About</h3>
+    <div className="about--container">
+      <h3 className="about--heading">About</h3>
       <p 
-        className="about-text">
-          I am a software developer aiming to solve real-world problems and make people's lives better.
+        className="about--text">
+          I am a frontend developer aiming to solve real-world problems and make people's lives better.
           I prioritze best practices in my current and future work while openly seeking out new challenges and things to learn.
       </p>
     </div>
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Interests() {
   return (
-    <div className="interests-container">
-      <h3 className="interests-heading">Interests</h3>
+    <div className="interests--container">
+      <h3 className="interests--heading">Interests</h3>
       <p 
-        className="interests-text">
+        className="interests--text">
           My goal is to get into full stack development.
           However, I enjoy reading about and researching different technologies like machine learning, AI, and blockchain technologies including Ethereum and Solidity.
           On the personal side, I like to swim, bike and run!
