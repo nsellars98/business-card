c619b6a950c8f09c7dd01658df4374615affe871
Nathan C. Sellars <nsellars98@gmail.com>
2022-02-21 15:25:14 -0600

    Updated project with finalized typography changes, new content to About and Interests sections

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,11 @@ function About() {
       <h3 className="about--heading">About</h3>
       <p 
         className="about--text">
-          I am a frontend developer aiming to solve real-world problems and make people's lives better.
-          I prioritze best practices in my current and future work while openly seeking out new challenges and things to learn.
+          I am a frontend developer with experience developing websites and web applications.
+          I have an Associate of Applied Science degree in computer programming with 3+ years of overall coding experience but no prior experience working for a company.<br />
+          <br />
+          My goals as a developer are aiming to solve real-world problems while striving to make people's lives better and easier through software, and I openly want to work with organizations that align with this philosophy.
+          My current and future work will prioritize best practices and code readability, and I continuously seek out new challenges and things to learn.
       </p>
     </div>
   );
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -6,9 +6,11 @@ function Interests() {
       <h3 className="interests--heading">Interests</h3>
       <p 
         className="interests--text">
-          My goal is to get into full stack development.
-          However, I enjoy reading about and researching different technologies like machine learning, AI, and blockchain technologies including Ethereum and Solidity.
-          On the personal side, I like to swim, bike and run!
+          My recent and current work has been on the frontend, however, my long-term goal is to work on the full stack.
+          I put a high importance on being well-rounded and the aspect of a jack-of-all-trades developer is very desirable to me.<br />
+          <br />
+          When I am not coding, I enjoy reading about and researching different technologies like machine learning, AI, and the blockchain.
+          On the personal side, I like to get outdoors as much as possible and spend time with family and friends.
       </p>
     </div>
   );
